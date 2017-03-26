617ad9599212a2e3147009307c7aaa758a4a23bf
AdityaTolety <aditya.srinivas2000@gmail.com>
2017-03-26 19:37:14 +0530

    Print 'Hello World'
    Display the message ‘Hello World’ in the Emulator.
    Submitting the first Assignment

---
+++ app/src/main/java/com/android/adityatolety/assignments/Assignment_1.java
@@ -0,0 +1,13 @@
+package com.android.adityatolety.assignments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Assignment_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment_1);
+    }
+}
